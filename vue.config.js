aafd8f667ba4334b755d297e43b8d0508e0050cf
Justin Lee <jlviper44@csu.fullerton.edu>
2025-03-25 21:38:21 -0700

    250325 -Updated proxy settings

--- vue.config.js
+++ vue.config.js
@@ -1,18 +1,18 @@
 module.exports = {
-  // devServer: {
-  //   "proxy": {
-  //     '/api': {
-  //       target: 'https://login.affluentco.com',
-  //       changeOrigin: true,
-  //       pathRewrite: {
-  //         '^/api': ''
-  //       }
-  //     },
-  //   }
-  // },
   pluginOptions: {
     vuetify: {
 			// https://github.com/vuetifyjs/vuetify-loader/tree/next/packages/vuetify-loader
 		}
   },
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://login.affluentco.com',
+        changeOrigin: true,
+        pathRewrite: {
+          '^/api': ''
+        }
+      }
+    }
+  }
 }
